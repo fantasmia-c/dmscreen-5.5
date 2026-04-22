1e122dccb0b19c9639a055983f8c90c817a87f09
fantasmia-c <kenjisancheskurogi@gmail.com>
2026-04-21 22:21:13 -0300

    Revise Exhaustion condition details in conditions.js
    Updated the Exhaustion condition description to include detailed effects and removal process.

--- src/tables/conditions.js
+++ src/tables/conditions.js
@@ -25,16 +25,7 @@ export const conditions = () => {
         </tr>
         <tr>
           <td>Exhaustion</td>
-          <td>
-            <ol>
-              <li>Disadvantage on ability checks.</li>
-              <li>Speed is halved.</li>
-              <li>Disadvantage on attack rolls and saving throws.</li>
-              <li>Max HP is halved.</li>
-              <li>Speed is reduced to 0.</li>
-              <li>Death.</li>
-            </ol>
-          </td>
+          <td> While you have the Exhaustion condition, you experience the following effects Exhaustion Levels. This condition is cumulative. Each time you receive it, you gain 1 Exhaustion level. You die if your Exhaustion level is 6. D20 Tests Affected. When you make a D20 Test, the roll is reduced by 2 times your Exhaustion level. Speed Reduced. Your Speed is reduced by a number of feet equal to 5 times your Exhaustion level. Removing Exhaustion Levels. Finishing a Long Rest removes 1 of your Exhaustion levels. When your Exhaustion level reaches 0, the condition ends</td>
         </tr>
         <tr>
           <td>Falling</td>
